9fb8a3ef46e65f2986187e42625e44af1c95a1d2
신현일/클라우드QualityEngineering팀/NE <hyunil.shin@nhn.com>
2021-12-26 21:25:39 +0900

    Update build_image.sh

--- testcafe/build_image.sh
+++ testcafe/build_image.sh
@@ -4,4 +4,5 @@
 # ../build_image.sh
 
 # --no-cache
+echo "docker build $TAG ${NODE_VERSION}"
 docker build -t anyonecantest/testcafe:$TAG-${NODE_VERSION} --build-arg TAG=$TAG --build-arg NODE_VERSION=${NODE_VERSION} .
