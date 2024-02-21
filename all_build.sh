0316fe05c91667e7eb458422190e97b88d279e40
고준영/QA1팀/EP <45865819+kjy861105@users.noreply.github.com>
2024-02-21 17:10:20 +0900

    Update all_build.sh-4.16.1-20231219

--- all_build.sh
+++ all_build.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-export TAG="4.16.1-20231212"
-# https://github.com/SeleniumHQ/docker-selenium/releases/tag/4.16.1-20231212
+export TAG="4.16.1-20231219"
+# https://github.com/SeleniumHQ/docker-selenium/releases/tag/4.16.1-20231219
 echo "build $TAG"
 
 ./build_image.sh
