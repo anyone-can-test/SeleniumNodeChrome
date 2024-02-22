f60986a41c175f3c37a69bc4acc2ba5d1509e9ab
고준영/QA1팀/EP <45865819+kjy861105@users.noreply.github.com>
2024-02-23 07:58:16 +0900

    Update all_build.sh_원복

--- all_build.sh
+++ all_build.sh
@@ -6,9 +6,6 @@ export TAG="4.18.0-20240220"
 # https://github.com/SeleniumHQ/docker-selenium/releases/tag/4.18.0-20240220
 echo "build $TAG"
 
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 40976EAF437D05B5
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32
-
 ./build_image.sh
 ./build_image_standalone.sh
 
