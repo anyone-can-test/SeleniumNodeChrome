abf3f3ecc2c976b88e187439bbc5cfe33bbd2497
고준영/QA1팀/EP <45865819+kjy861105@users.noreply.github.com>
2024-03-29 11:17:00 +0900

    [TestCafe] SeleniumNodeChrome 신규 버전 이미지 추가 (#16)
    * Update all_build.sh
    
    * Update all_build.sh
    
    * Update all_build.sh
    
    * Update all_build.sh
    
    * Update all_build.sh
    
    * Update all_build.sh
    
    * testcafe_docker_4.15.0-20231102
    
    node 20
    
    * Update all_build.sh_4.15.0-20231108
    
    * Update all_build.sh-4.16.0-20231206
    
    * Update all_build.sh-4.16.1-20231208
    
    * Update all_build.sh-4.16.1-20231212
    
    * Update all_build.sh-4.16.1-20231219
    
    * Update all_build.sh-4.17.0-20240123
    
    * Update all_build.sh-4.18.0-20240220
    
    * Update all_build.sh_누락 키 등록
    
    * Update all_build.sh_원복
    
    * Update docker-publish2.yml_test
    
    키 추가
    
    * Update docker-publish2.yml
    
    Update docker-publish2.yml 원복
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile_standalone
    
    키 추가
    
    * Update all_build.sh_4.17.0-20240123
    
    * Update all_build.sh
    
    * Update Dockerfile
    
    수정
    
    * Update Dockerfile_standalone
    
    수정

--- all_build.sh
+++ all_build.sh
@@ -2,20 +2,17 @@
 
 set -e
 
-
-export TAG="4.17.0-20240123"
-# https://github.com/SeleniumHQ/docker-selenium/releases/tag/4.17.0-20240123
+export TAG="4.18.0-20240220"
+# https://github.com/SeleniumHQ/docker-selenium/releases/tag/4.18.0-20240220
 echo "build $TAG"
-
 
 ./build_image.sh
 ./build_image_standalone.sh
 
 pushd testcafe
-export NODE_VERSION=18
+export NODE_VERSION=20
 ./build_image.sh
 popd
-
 
 # push to registry
 
