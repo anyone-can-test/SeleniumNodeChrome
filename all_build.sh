49136a151ecc2291c71bf5313615fbe4842691c9
신현일/클라우드품질관리실/NHN Cloud <hyunil.shin@nhn.com>
2025-06-04 10:49:09 +0900

    Update all_build.sh

--- all_build.sh
+++ all_build.sh
@@ -6,12 +6,14 @@ set -e
 export TAG="4.22.0-20240621"
 # https://github.com/SeleniumHQ/docker-selenium/releases/tag/4.18.0-20240220
 echo "build $TAG"
+echo "TAG=${TAG}" >> $GITHUB_ENV
 
 ./build_image.sh
 ./build_image_standalone.sh
 
 pushd testcafe
 export NODE_VERSION=20
+echo "NODE_VERSION=${NODE_VERSION}" >> $GITHUB_ENV
 ./build_image.sh
 popd
 
