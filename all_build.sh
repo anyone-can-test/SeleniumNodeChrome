d39f700e5fd5c0ca655039f828174ea4e3807777
신현일/클라우드QualityEngineering팀/CL <hyunil.shin@nhn.com>
2025-06-04 10:53:39 +0900

    update

--- all_build.sh
+++ all_build.sh
@@ -6,12 +6,14 @@ set -e
 export TAG="134.0-20250525"
 # https://github.com/SeleniumHQ/docker-selenium/releases/tag/4.18.0-20240220
 echo "build $TAG"
+cho "TAG=${TAG}" >> $GITHUB_ENV
 
 ./build_image.sh
 ./build_image_standalone.sh
 
 pushd testcafe
 export NODE_VERSION=20
+echo "NODE_VERSION=${NODE_VERSION}" >> $GITHUB_ENV
 ./build_image.sh
 popd
 
